5d7e91aafde12d6d03d6a99ee015dc7c66f6eae0
dell\irving <irvin_ac3@hotmail.com>
2018-07-22 13:26:59 -0500

    carpeta views

--- chat-react/Views/panel.ts
+++ chat-react/Views/panel.ts
@@ -1,14 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View,Image } from 'react-native';
 
-export default class App extends React.Component {
+export default class Panel extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-       <Image style={{width:50,height: 50}}
-       source={require('./img/chatwhite.png')}
-       // source={{uri:'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
-        />
+        <Text>Open up fucking App.js to start working on your app!</Text>
+        
       </View>
     );
   }
@@ -17,7 +15,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
