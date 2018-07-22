9c7e73a610e75b8f3b9ebf8edf5d2f777ab2b463
dell\irving <irvin_ac3@hotmail.com>
2018-07-22 14:38:04 -0500

    Login

--- chat-react/App.js
+++ chat-react/App.js
@@ -1,15 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View,Image } from 'react-native';
+import { StyleSheet, Text, View,Image,Button,TextInput,KeyboardAvoidingView,ToastAndroid } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      titleText: "Iniciar sesion",
+      bodyText: 'This is not really a bird nest.',
+      user:"",
+      pass:""
+    };
+  }
+
+  onEnter(){
+    console.log("Fierro");
+    ToastAndroid.show('A pikachu appeared nearby !', ToastAndroid.SHORT);
+  }
+
   render() {
     return (
+     
       <View style={styles.container}>
-       <Image style={{width:50,height: 50}}
+       
+        <KeyboardAvoidingView 
+         style={styles.container}
+         behavior="padding"
+        >
+         <Image style={{width:50,height: 50}}
        source={require('./img/chatwhite.png')}
        // source={{uri:'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
         />
+        <Text style={styles.titleText} >
+          {this.state.titleText}{'\n'}{'\n'}
+        </Text>
+        <TextInput
+        style={{height: 40,width:150, borderColor: 'gray', 
+        borderWidth: 1,color:"white",padding:10,margin:10}}
+        onChangeText={(text) => this.setState({user:text})}
+        value={this.state.user}
+        underlineColorAndroid='transparent'
+        placeholder="usuario"
+        />
+        
+        <TextInput
+        style={{height: 40,width:150, borderColor: 'gray', 
+        borderWidth: 1,color:"white",padding:10,margin:10}}
+        onChangeText={(text) => this.setState({pass:text})}
+        value={this.state.pass}
+        underlineColorAndroid='transparent'
+        placeholder="password"
+        />
+        <Button
+          onPress={this.onEnter}
+          title="Entrar"
+          color="#0066ff"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        </KeyboardAvoidingView >
+        
+      
+
+     
       </View>
+      
     );
   }
 }
@@ -20,5 +73,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color:"white"
   },
 });
